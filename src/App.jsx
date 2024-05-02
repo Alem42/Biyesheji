762074030840397a22b01eb251e46e9aeaed1ff6
Alem42 <915987620@qq.com>
2024-05-03 04:36:00 +0800

    vite config change

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Page1 from "./pages/Page1";
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -8,26 +12,17 @@ function App() {
 
   return (
     <>
+      <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Link to="/">Home</Link>{` | `}  
+        <Link to="/page">Page1</Link> 
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page" Component={Page1} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </Router>
     </>
   )
 }
---
+++ src/pages/Page1.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Page1() {
+    return (
+        <>
+            <h1>Hello Page1</h1>
+        </>
+    )
+}
