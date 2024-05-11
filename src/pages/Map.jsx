b801ca21feec63a111de9e1603fac4bf183ddd46
Alem42 <915987620@qq.com>
2024-05-11 20:31:05 +0800

    0.1.6

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,7 +1,34 @@
-export default function Page1() {
-  return (
-      <>
-          <p className="font-bold text-2xl m-10">Hello Page1</p>
-      </>
-  )
-}
+// src/App.jsx
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+    const [location, setLocation] = useState({ latitude: 39.915, longitude: 116.404 });
+
+    useEffect(() => {
+        const map = new BMapGL.Map("mapContainer"); 
+        const point = new BMapGL.Point(location.longitude, location.latitude);
+        map.centerAndZoom(point, 15);
+        map.enableScrollWheelZoom(true);
+        const marker = new BMapGL.Marker(point);
+        map.addOverlay(marker);
+
+        const fetchLocation = async () => {
+            //const response = await fetch('http://localhost:3000/location');
+            //const data = await response.json();
+            const data = {
+              "latitude": 30.6574,
+              "longitude": 104.0658
+            }
+            setLocation({ latitude: data.latitude, longitude: data.longitude });
+            map.panTo(new BMapGL.Point(data.longitude, data.latitude));
+        };
+
+        const intervalId = setInterval(fetchLocation, 5000); // 每5秒钟获取一次位置
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    return <div id="mapContainer" style={{ height: '100vh', width: '900%' }} />;
+};
+
+export default App;
--- src/utils/host.jsx
+++ src/utils/host.jsx
@@ -1,5 +1,6 @@
 // host.jsx
 const server_host = import.meta.env.VITE_SERVER_HOST;
 const loginApi = server_host + '/login';
-
+console.log(server_host);
+console.log(loginApi);
 export { loginApi };
